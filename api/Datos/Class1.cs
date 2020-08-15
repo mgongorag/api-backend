b6d70a6da202043b2432a5313a3104cea936272e
= <imikesb@users.noreply.github.com>
2020-08-15 11:53:32 -0600

    Se creo la conexion

---
+++ api/Datos/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Funciones
+    {
+        public string lblStatus = "";
+        private string codigoSeguridad = "m1gU3l";
+
+        public string SeguridadSHA521(string pass)
+        {
+            System.Security.Cryptography.SHA512Managed HashhTool = new System.Security.Cryptography.SHA512Managed();
+            Byte[] HashByte = Encoding.UTF8.GetBytes(string.Concat(pass, codigoSeguridad));
+            Byte[] EncryptedByte = HashhTool.ComputeHash(HashByte);
+            HashhTool.Clear();
+
+            return Convert.ToBase64String(EncryptedByte);
+        }
+
+    }
+}
---
+++ api/Datos/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        private static string usuario = "sa";
+        private static string password = "admin";
+        private static string servidor = "GODLIKE\\SQLEXPRESS";
+        private static string DB = "DBEvaluacion";
+
+        public static string CadenaConexionSQL()
+        {
+            return "Persist Security Info = false; User ID = '" + usuario
+                        + "'; Password = '" + password
+                        + "'; Initial Catalog = '" + DB
+                        + "'; Server = '" + servidor + "'";
+        }
+
+    }
+
+    
+}
