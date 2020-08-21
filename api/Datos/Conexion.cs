00fcdd05fbf5bf02850c8ecb30301a2dfc363086
= <imikesb@users.noreply.github.com>
2020-08-20 21:54:56 -0600

    Se agregaron los scripts de la creacion de la base de datos y procedimientos almacenados, se implemento en la API Funciones asi como la conexion a la DB, Se agrego controlador

--- api/Datos/Conexion.cs
+++ api/Datos/Conexion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
@@ -10,17 +12,53 @@ namespace Datos
 {
     public class Conexion
     {
-        private static string usuario = "sa";
+        private static string usuario = "admin";
         private static string password = "admin";
         private static string servidor = "GODLIKE\\SQLEXPRESS";
         private static string DB = "DBEvaluacion";
 
-        public static string CadenaConexionSQL()
+        public static string obtenerCadenaConexionSQL()
         {
             return "Persist Security Info = false; User ID = '" + usuario
                         + "'; Password = '" + password
                         + "'; Initial Catalog = '" + DB
                         + "'; Server = '" + servidor + "'";
+        }
+
+        //EJECTURAR PROCEDIMIENTO ALMACENADO PASANDOLO COMO PARAMETRO
+        public static SqlCommand crearComandoProc(String SP)
+        {
+            string cadenaConexion = Conexion.obtenerCadenaConexionSQL();
+            SqlConnection miConexion = new SqlConnection(cadenaConexion);
+            SqlCommand comando = new SqlCommand(SP, miConexion);
+            comando.CommandType = CommandType.StoredProcedure;
+            return comando;
+        }
+        
+
+        /*
+         * EjecutarComandoSelect
+         */
+        public static DataTable ejecutarComandoSelect(SqlCommand comando)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                comando.Connection.Open();
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                comando.Connection.Dispose();
+                comando.Connection.Close();
+            }
+            return dt;
         }
 
     }
---
+++ api/Entidades/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        public int idUsuario { get ; set; }
+        public string TxtNombres { get; set; }
+        public string TxtApellidos { get ; set; }
+        public string TxtDireccion { get ; set; }
+        public string TxtEmail { get; set; }
+        public string TxtPassword { get ; set; }
+        public string FechaIngreso { get ; set; }
+        public int IntEstado { get ; set; }
+
+    }
+}
